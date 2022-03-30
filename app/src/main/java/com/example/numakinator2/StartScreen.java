7b66468e47925ad63bf0dbbd3e0383a327e6193b
fozithebear <fozirulez@gmail.com>
2022-03-30 14:32:40 -0400

    Changed values of interval edittext and guesses edit text

--- app/src/main/java/com/example/numakinator2/StartScreen.java
+++ app/src/main/java/com/example/numakinator2/StartScreen.java
@@ -3,6 +3,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,12 +11,14 @@ public class StartScreen extends AppCompatActivity {
 
     Button nextActivity;
     Button prevActivity;
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
 
+        name = (EditText) findViewById(R.id.playerName);
         nextActivity = (Button) findViewById(R.id.startGame);
         prevActivity = (Button) findViewById(R.id.backButton);
 
