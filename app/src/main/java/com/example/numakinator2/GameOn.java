ca80346285a90ba575c0a90d02d0d8cc44d2e273
Khoa Vu <khoavu220997@gmail.com>
2022-04-02 16:49:43 -0400

    animations

--- app/src/main/java/com/example/numakinator2/GameOn.java
+++ app/src/main/java/com/example/numakinator2/GameOn.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.HashMap;
